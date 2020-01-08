8bf65a73d4501c3fed67839c62e85b2f9a4a585a
Manuel Barkhau <mb@admetrics.io>
2020-01-08 17:40:25 +0000

    updates to work with bigquery

--- query_db.py
+++ query_db.py
@@ -1,6 +1,13 @@
-import io
+# -*- coding: utf-8 -*-
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import os
 import re
 import time
+import tempfile
 import threading
 import subprocess
 
@@ -8,32 +15,53 @@ import sublime
 import sublime_plugin
 
 
-# https://regex101.com/r/hO3eH6
-DB_URL_RE = re.compile(r"""
+PROGRESS = "⡇⣆⣰⢸⠹⠏"
+
+
+# https://regex101.com/r/hO3eH6/10
+CONNECT_URL_RE = re.compile(r"""
 ^
-    (?P<connector>[^:]+)
-    ://
+(?P<scheme>[^:]+)
+://
+(?:
     (?:
+        (?P<user>[^:@]+)
+        @
+        (?P<host>[^:/]+)
         (?:
-            (?P<username>[^:@]+)
-            @
-            (?P<host>[^:/]+)
-            (?:
-                \:
-                (?P<port> \d+)
-            )?
+            \:
+            (?P<port> \d+)
         )?
-        (?: (?P<database> \/ .+))
-    )
+    )?
+    (?:\/ (?P<db> .+))?
+)
 $
-""", re.VERBOSE)
+""", flags=re.VERBOSE)
 
 
-def parse_credentials_url(url):
-    match = DB_URL_RE.match(url)
+QUERY_DB_URL_RE = re.compile(r"^--\s?db(?:_url)?=(.*)$", flags=re.MULTILINE)
+
+QUERY_DB_ID_RE = re.compile(r"^--\s?db=(\w+)$", flags=re.MULTILINE)
+
+
+def parse_db_url(url):
+    match = CONNECT_URL_RE.match(url)
     if match is None:
-        raise ValueError("Invalid URL for db connecion: '{}'".format(url))
+        errmsg = "Invalid URL for db connecion: '{}'".format(url)
+        raise ValueError(errmsg)
     return match.groupdict()
+
+
+def mk_time_str(duration):
+    if duration < 1:
+        return "{:}ms".format(int(duration * 1000))
+
+    if duration < 60:
+        return "{:5.1f}s".format(duration)
+
+    mins = int(duration) // 60
+    secs = duration % 60
+    return "{:02}:{:02} min".format(mins, round(secs))
 
 
 class StatusUpdateThread(threading.Thread):
@@ -45,35 +73,37 @@ class StatusUpdateThread(threading.Thread):
         super(StatusUpdateThread, self).__init__()
 
     def status_id(self, thread):
-        return "psql_query_{}".format(hex(id(thread)))
+        return "db_query_{}".format(hex(id(thread)))
 
     def erase_status(self, thread):
         thread.view.erase_status(self.status_id(thread))
 
     def update_status(self, thread):
         totaltime = (thread.t2 or time.time()) - thread.t0
-        if totaltime < 1:
-            totaltime_str = "{:}ms".format(int(totaltime * 1000))
-        else:
-            totaltime_str = "{:.3f}s".format(totaltime)
+        totaltime_str = mk_time_str(totaltime)
+
+        indicator_tick = int(totaltime * 10) % len(PROGRESS)
+        indicator = PROGRESS[indicator_tick]
+
         thread.view.set_status(self.status_id(thread), (
-            "[psql {:} read {}]"
-        ).format(totaltime_str, 0))
+            "[db query {} {:} read {}]"
+        ).format(indicator, totaltime_str, 0))
 
     def run(self):
         while True:
             if self.queries:
                 running_query_threads = []
                 for t in self.queries:
-                    is_alive = not t.t2 or (time.time() - t.t2) < self.status_linger
-                    if is_alive:
+                    is_alive = not t.t2
+                    is_linger = t.t2 and (time.time() - t.t2) < self.status_linger
+                    if is_alive or is_linger:
                         running_query_threads.append(t)
                         self.update_status(t)
                     else:
                         self.erase_status(t)
                 self.queries = running_query_threads
 
-            time.sleep(.07)
+            time.sleep(0.1)
 
 
 STATUS_UPDATE_THREAD = StatusUpdateThread()
@@ -83,142 +113,166 @@ STATUS_UPDATE_THREAD.start()
 
 class QueryThread(threading.Thread):
 
-    def __init__(self, query, window, view=None):
+    def __init__(self, query, window, settings, view=None):
         self.query = query
         self.window = window
+        self.settings = settings
         self.view = view
         self.t0 = time.time()
         self.t1 = None
         self.t2 = None
         super(QueryThread, self).__init__()
 
+    def parse_db_connect_params(self, query):
+        db_url_match = QUERY_DB_URL_RE.search(self.query)
+        if db_url_match:
+            db_url = db_url_match.group(1)
+        else:
+            db_id_match = QUERY_DB_ID_RE.search(self.query)
+            if db_id_match:
+                db_id = db_id_match.group(1)
+                if db_id in self.settings['urls']:
+                    db_url = self.settings['urls'][db_id]
+                else:
+                    raise KeyError("Invalid -- db={}".format(db_id))
+            else:
+                db_url = self.settings['urls'].get('default')
+
+        if db_url:
+            return parse_db_url(db_url)
+        else:
+            raise Exception("No DB Connection Configured")
+
+    def query_file(self):
+        query = self.query
+        if not query.endswith(";"):
+            query += ";"
+
+        fobj = tempfile.NamedTemporaryFile(mode="wt", encoding='utf-8', suffix=".sql")
+        fobj.write(query)
+        fobj.flush()
+        return fobj
+
     def run(self):
-        # TODO (mb 2016-05-25): get configuration from
-        #   plugin config file, or parse it from the query
-        user = "ddmuser"
-        port = "5439"
-        db = "ddm"
-        host = "consul.nt.vc"
-        # db = "adm"
-        # host = "senator.nt.vc"
-        # language = "sql"
+        try:
+            db_connect_params = self.parse_db_connect_params(self.query)
+            scheme = db_connect_params['scheme']
 
-        # config_path = "/tmp/.pgpass"
-        # with io.open(config_path, encoding='utf-8', mode='w') as fh:
-        #     fh.write("\n".join([
-        #         "user=" + user,
-        #         "password=" + password,
-        #         "language=" + language,
-        #     ]))
+            PSQL_SCHEMES = ("pg", "psql", "postgres", "postgresql", "rs", "redshift")
+            BIGQUERY_SCHEMES = ("bq", "bigquery")
 
-        query_path = "/tmp/tmp_pgsql_query.sql"
-        with io.open(query_path, encoding='utf-8', mode='w') as fh:
-            query = self.query
-            if not query.endswith(";"):
-                query += ";"
-            fh.write(query)
+            env = os.environ.copy()
+            with self.query_file() as fobj:
+                if scheme in PSQL_SCHEMES:
+                    cmd = [
+                        self.settings['executables']['psql'],
+                        "--no-password",
+                        "--host", db_connect_params['host'],
+                        "--port", db_connect_params['port'],
+                        "--user", db_connect_params['user'],
+                        "--dbname", db_connect_params['db'],
+                        "--file", fobj.name,
+                        # "--output", out_path,
+                    ]
+                    env['PGCONNECT_TIMEOUT'] = "3"
+                    shell = False
+                elif scheme in BIGQUERY_SCHEMES:
+                    bq_cmd = self.settings['executables']['bq']
+                    cmd = "cat {} | {} query --nouse_legacy_sql".format(fobj.name, bq_cmd)
+                    if db_connect_params['host']:
+                        cmd += " --project_id=" + db_connect_params['host']
+                    if db_connect_params['db']:
+                        cmd += " --dataset_id=" + db_connect_params['db']
 
-        out_path = "/tmp/tmp_pgsql_output_{}.txt".format(id(self))
+                    env['CLOUDSDK_ACTIVE_CONFIG_NAME'] = db_connect_params['user']
+                    shell = True
+                else:
+                    errmsg = "Unknown scheme: {}".format(scheme)
+                    raise Exception(errmsg)
 
-        self.proc = subprocess.Popen(
-            [
-                "psql",
-                "--no-password",
-                "--host", host,
-                "--port", port,
-                "--user", user,
-                "--dbname", db,
-                "--file", query_path,
                 # TODO (mb 2016-08-19): write to out path and check
                 #   its file size instead of doing proc.communicate()
-                # "--output", out_path,
-            ],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            # env=env,
-        )
+                # out_path = "/tmp/tmp_pgsql_output_{}.txt".format(id(self))
 
-        # TODO (mb 2016-05-23): trunkate large output and write to
-        #   separate file.
-        out_buffer = []
-        err_buffer = []
+                self.proc = subprocess.Popen(
+                    cmd,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    env=env,
+                    shell=shell,
+                )
 
-        stdout_value, stderr_value = self.proc.communicate()
-        t_first_bytes = time.time()
-        out_buffer.append(stdout_value.decode('utf-8'))
-        err_buffer.append(stderr_value.decode('utf-8'))
+                # TODO (mb 2016-05-23): truncate large output and write to
+                #   separate file.
+                out_buffer = []
+                err_buffer = []
 
-        ret_code = self.proc.wait()
+                stdout_value, stderr_value = self.proc.communicate()
+                ttfb = time.time()
+                out_buffer.append(stdout_value.decode('utf-8'))
+                err_buffer.append(stderr_value.decode('utf-8'))
 
-        # TODO (mb 2016-08-24): write to file and check st_mtime/st_size
-        #   rather than reading from stdout.
-        self.t1 = time.time()
+                ret_code = self.proc.wait()
 
-        output = ""
-        if ret_code == 0:
-            output += (
+            # TODO (mb 2016-08-24): write to file and check st_mtime/st_size
+            #   rather than reading from stdout.
+            self.t1 = time.time()
+
+            response_time = (ttfb - self.t0)
+            total_time = (time.time() - self.t0)
+            response_time_str = mk_time_str(response_time)
+            total_time_str = mk_time_str(total_time)
+
+            output = (
                 "-" * 20 + " QUERY " + "-" * 20 + "\n" +
                 self.query + "\n" +
                 "-" * 20 + " TIMING " + "-" * 20 + "\n" +
-                "Response Time: {:9.2f}ms\nTotal Time   : {:9.2f}ms\n".format(
-                    (t_first_bytes - self.t0) * 1000,
-                    (time.time() - self.t0) * 1000,
+                "Response Time: {:9.2f}ms ({})\nTotal Time   : {:9.2f}ms ({})\n".format(
+                    response_time * 1000, response_time_str,
+                    total_time * 1000, total_time_str,
                 ) +
                 "-" * 20 + " RESULT " + "-" * 20 + "\n" +
                 "".join(out_buffer).rstrip()
             )
 
-        if any(err_buffer) or ret_code:
-            output += (
-                "\n\nExit Code: " + str(ret_code) + "\n" +
-                "".join(err_buffer) + "\n" +
-                "-" * 48 + "\n"
-            )
+            if ret_code:
+                output += (
+                    "\n\nExit Code: " + str(ret_code) + "\n" +
+                    "".join(err_buffer) + "\n" +
+                    "-" * 48 + "\n"
+                )
 
-        self.t2 = time.time()
+            if self.view is None:
+                self.view = self.window.new_file()
+                self.view.set_syntax_file("Packages/SQL/SQL.sublime-syntax")
 
-        self.view.set_status("psql_running", "")
-
-        if self.view is None:
-            self.view = self.window.new_file()
-            self.view.set_syntax_file("Packages/SQL/SQL.sublime-syntax")
-
-        self.view.run_command("query_db_output", {
-            "output" : output
-        })
-
-
-# class PsqlDbQueryCommand(sublime_plugin.TextCommand):
-
-#     def run(self, edit):
-#         # TODO (mb 2016-05-25): if the selection is empty/not a valid
-#         # sql statement, extend the selection to see if it is within
-#         # a valid sql statement and execute that.
-#         sels = self.view.sel()
-#         for sel in sels:
-#             sel_text = self.view.substr(sel)
-#             break
-#         if not sel_text:
-#             # get whole text insted of selection
-#             sel_text = self.view.substr(sublime.Region(0, self.view.size()))
-#         if not sel_text:
-#             return
-
-#         query = sel_text.strip()
-
-#         t = QueryThread(query, self.view.window())
-#         t.start()
-#         STATUS_UPDATE_THREAD.queries.append(t)
+            self.view.run_command("query_db_output", {
+                "output" : output
+            })
+        finally:
+            self.t2 = time.time()
+            self.view.set_status("psql_running", "")
 
 
 class QueryDbCommand(sublime_plugin.TextCommand):
 
-    def run(self, edit, connection_id='default'):
+    def get_settings(self):
         settings = self.view.settings()
-        # print(settings.get('query_db_executables'))
-        # print(settings.get('query_db_connector_defaults'))
-        # print(settings.get('query_db_connection_urls'))
-        # print(settings.get('tab_size'))
+        executables = settings.get('query_db_executables').copy()
+        urls = settings.get('query_db_connection_urls').copy()
+
+        user_settings = sublime.load_settings("SublimeQueryDB.sublime-settings")
+        user_executables = user_settings.get('query_db_executables')
+        user_urls = user_settings.get('query_db_connection_urls')
+
+        if isinstance(user_executables, dict):
+            executables.update(user_executables)
+        if isinstance(user_urls, dict):
+            urls.update(user_urls)
+
+        return {'executables': executables, 'urls': urls}
+
+    def run(self, edit, connection_id='default'):
         sels = self.view.sel()
         for sel in sels:
             sel_text = self.view.substr(sel)
@@ -226,7 +280,6 @@ class QueryDbCommand(sublime_plugin.TextCommand):
 
         if not sel_text:
             # see if we are in a query file
-            in_place = True
             sel_text = self.view.substr(sublime.Region(0, self.view.size()))
             if not sel_text.startswith("-------------------- QUERY --------------------"):
                 return
@@ -240,7 +293,9 @@ class QueryDbCommand(sublime_plugin.TextCommand):
         query = query.split("-------------------- TIMING --------------------")[0]
         query = query.strip()
 
-        t = QueryThread(query, self.view.window(), view=self.view)
+        t = QueryThread(
+            query, self.view.window(), settings=self.get_settings(), view=self.view
+        )
         t.start()
         STATUS_UPDATE_THREAD.queries.append(t)
 
